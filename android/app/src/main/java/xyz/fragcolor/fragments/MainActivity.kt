d11b8aa3466ee792c54ffdc28683f51c2689bbbe
Guus Waals <_@guusw.nl>
2022-06-07 17:02:55 +0200

    Fix android build

--- android/app/src/main/java/xyz/fragcolor/fragments/MainActivity.kt
+++ android/app/src/main/java/xyz/fragcolor/fragments/MainActivity.kt
@@ -7,7 +7,7 @@ import org.libsdl.app.SDLActivity;
 class MainActivity : SDLActivity() {
     override fun getLibraries() : Array<String> {
         return arrayOf(
-            "fragments"
+            "app"
         )
     }
 
